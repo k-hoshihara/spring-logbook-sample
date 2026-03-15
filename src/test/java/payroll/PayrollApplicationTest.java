39efe4de32a67b81ef02f91fda009a1a30f9ff69
k-hoshihara <90707936+k-hoshihara@users.noreply.github.com>
2026-03-16 02:43:11 +0900

    SpringBootTestの実装

---
+++ src/test/java/payroll/PayrollApplicationTest.java
@@ -0,0 +1,12 @@
+package payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
